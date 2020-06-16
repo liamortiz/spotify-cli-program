15d3c7cbf0714932b2a490cf9bb83c537c5baf9a
Leamsi Escribano <leamsi.esc@gmail.com>
2020-06-16 15:28:07 -0400

    Fixed model relationships

--- lib/models/playlist.rb
+++ lib/models/playlist.rb
@@ -1,4 +1,5 @@
 class Playlist < ActiveRecord::Base
     belongs_to :user
+    has_many :records
     has_many :songs, through: :records
 end
--- lib/models/song.rb
+++ lib/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ActiveRecord::Base
+  has_many :records
   has_many :playlists, through: :records
 end
